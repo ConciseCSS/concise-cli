54086be84d4121dcd4cb15892d80aa06fed654c8
James Kolce <contact@jameskolce.com>
2016-05-31 16:37:45 -0600

    [ref: #1] Delete custom media tests

--- test/index.js
+++ test/index.js
@@ -13,11 +13,6 @@ const expected = (file) => {
 test('@media', (t) => {
 
   t.equal(
-    actual('customMedia'),
-    expected('customMedia'),
-    'should transform custom media queries');
-
-  t.equal(
     actual('mediaMinMax'),
     expected('mediaMinMax'),
     'should transform ranges in media queries');
